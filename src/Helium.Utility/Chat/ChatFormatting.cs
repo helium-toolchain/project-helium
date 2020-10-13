43c451d9a7dca092d727025159bd7aa31f1eed38
ExaInsanity <61018569+ExaInsanity@users.noreply.github.com>
2020-10-13 10:34:35 +0200

    Add chatformatting enum

---
+++ src/Helium.Utility/Chat/ChatFormatting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helium.Utility.Chat
+{
+    [Flags]
+    public enum ChatFormatting
+    {
+        Black = 1,
+        DarkBlue = 2,
+        DarkGreen = 4,
+        Cyan = 8,
+        DarkRed = 16,
+        DarkPurple = 32,
+        Orange = 64,
+        Gray = 128,
+        Blue = 256,
+        Green = 1024,
+        Aqua = 2048,
+        Red = 4096,
+        LightPurple = 8192,
+        Yellow = 16384,
+        White = 32768,
+
+        Obfuscated = 65536,
+        Bold = 131072,
+        Strikethrough = 262144,
+        Underline = 524288,
+        Italic = 1048576
+    }
+}
