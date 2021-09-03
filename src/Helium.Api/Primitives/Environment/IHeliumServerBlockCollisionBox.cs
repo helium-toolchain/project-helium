b5356cf9d7563c07f0c1a16b39901f8c9ca67bc5
ExaInsanity <nicolaiandorfer@outlook.com>
2021-09-03 21:44:19 +0200

    add collision box

--- src/Helium.Api/Primitives/Environment/IHeliumServerBlockCollisionBox.cs
+++ src/Helium.Api/Primitives/Environment/IHeliumServerBlockCollisionBox.cs
@@ -1,5 +1,13 @@
 ﻿namespace Helium.Api.Primitives.Environment;
 
+/// <summary>
+/// Secondary hitbox, for collisions rather than interactions
+/// </summary>
 public interface IHeliumServerBlockCollisionBox
 {
+	/// <summary>
+	/// Sub-Hitboxes this hitbox consists of. 
+	/// For the Helium network protocol, these can overlap; for the Notchian network protocol these cannot overlap.
+	/// </summary>
+	public IHeliumServerSingleBlockHitbox[] SubHitboxes { get; set; }
 }
