810b90ce7498f201aefca60924241d91d38ab637
ExaInsanity <irisfeanora@outlook.com>
2021-09-26 10:27:33 +0200

    add raw string support
    some packets don't use VarInt-prefixed strings, therefore we need raw string support too

--- src/Helium.Network.Api.Vanilla/PacketStream.cs
+++ src/Helium.Network.Api.Vanilla/PacketStream.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Buffers.Binary;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -643,6 +644,26 @@ public class PacketStream : Stream
 		return await this.ReadUnsignedByteAsync() / 256 * 360;
 	}
 
+	/// <summary>
+	/// Reads a raw string of specified length from the current stream.
+	/// </summary>
+	public String ReadRawString(Int32 length)
+	{
+		Span<Byte> buffer = new Byte[length];
+		BaseStream.Read(buffer);
+		return Encoding.UTF8.GetString(buffer);
+	}
+
+	/// <summary>
+	/// Reads a raw string of specified length from the current stream asynchronously.
+	/// </summary>
+	public async Task<String> ReadRawStringAsync(Int32 length)
+	{
+		Byte[] buffer = new Byte[length];
+		await BaseStream.ReadAsync(buffer);
+		return Encoding.UTF8.GetString(buffer);
+	}
+
 	#endregion
 
 	#region Writing primitives
@@ -1070,6 +1091,24 @@ public class PacketStream : Stream
 	{
 		BaseStream.Write(MemoryMarshal.Cast<Double, Byte>(val.AsSpan()));
 		return Task.CompletedTask;
+	}
+
+	/// <summary>
+	/// Writes a raw string to the current stream.
+	/// </summary>
+	public void WriteRawString(String val)
+	{
+		Span<Byte> buffer = Encoding.UTF8.GetBytes(val);
+		BaseStream.Write(buffer);
+	}
+
+	/// <summary>
+	/// Writes a raw string to the current stream asynchronously.
+	/// </summary>
+	public async Task WriteRawStringAsync(String val)
+	{
+		Byte[] buffer = Encoding.UTF8.GetBytes(val);
+		await BaseStream.WriteAsync(buffer);
 	}
 
 	#endregion
