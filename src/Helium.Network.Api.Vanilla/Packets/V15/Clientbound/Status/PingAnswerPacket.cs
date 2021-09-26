13b7a94186f3b45a548d838482938b3ea2936cab
ExaInsanity <irisfeanora@outlook.com>
2021-09-26 10:53:25 +0200

    implement first few packets

---
+++ src/Helium.Network.Api.Vanilla/Packets/V15/Clientbound/Status/PingAnswerPacket.cs
@@ -0,0 +1,30 @@
+﻿namespace Helium.Network.Api.Vanilla.Packets.V15.Clientbound.Status;
+
+using System;
+
+using Helium.Api.Mojang;
+
+/// <summary>
+/// Status-C0x01: Clientbound Ping answer packet, identifier 0x01
+/// </summary>
+public class PingAnswerPacket : IPacket
+{
+	public VarInt Id => 0x01;
+
+	/// <summary>
+	/// Int64 payload; should be the same as sent by the client in S0x01
+	/// </summary>
+	public Int64 Payload { get; set; }
+
+	public void Read(PacketStream stream)
+	{
+		this.Payload = stream.ReadInt64();
+	}
+
+	public void Write(PacketStream stream)
+	{
+		stream.WriteVarInt(Id.Length + 8);
+		stream.WriteVarInt(Id);
+		stream.WriteInt64(Payload);
+	}
+}
