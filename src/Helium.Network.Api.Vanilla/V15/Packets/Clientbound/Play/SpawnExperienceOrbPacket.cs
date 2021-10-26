9e251cb496269a5d9ede09267146de1ecf4b5ecb
ExaInsanity <irisfeanora@outlook.com>
2021-10-26 20:23:52 +0200

    implement C0x01 and C0x02

---
+++ src/Helium.Network.Api.Vanilla/V15/Packets/Clientbound/Play/SpawnExperienceOrbPacket.cs
@@ -0,0 +1,63 @@
+﻿namespace Helium.Network.Api.Vanilla.V15.Packets.Clientbound.Play;
+
+using System;
+
+using Helium.Api.Mojang;
+
+/// <summary>
+/// C0x01: Clientbound spawn experience orb packet.
+/// Spawns exactly one experience orb.
+/// </summary>
+
+// this is listed on wiki.vg as spawning one or more orbs, see https://wiki.vg/index.php?title=Protocol&oldid=15871#Spawn_Experience_Orb,
+// but that does not appear to be the case considering its lack of a count property. In fact, any overrolling mechanic is nullified by this
+// packet sending the entity ID for... some reason. It can be assumed this specific wiki.vg revision is mistaken.
+public struct SpawnExperienceOrbPacket : IPacket
+{
+	public VarInt Id => 0x01;
+
+	/// <summary>
+	/// Server-side entity ID of this experience orb.
+	/// </summary>
+	public VarInt EntityID { get; set; }
+
+	/// <summary>
+	/// Stores the amount of experience this orb rewards upon collection.
+	/// </summary>
+	public Int16 Value { get; set; }
+	
+	/// <summary>
+	/// Current position of this experience orb.
+	/// </summary>
+	public Double PositionX { get; set; }
+
+	/// <summary>
+	/// Current position of this experience orb.
+	/// </summary>
+	public Double PositionY { get; set; }
+
+	/// <summary>
+	/// Current position of this experience orb.
+	/// </summary>
+	public Double PositionZ { get; set; }
+
+	public void Read(PacketStream stream)
+	{
+		this.EntityID = stream.ReadVarInt();
+		this.PositionX = stream.ReadDouble();
+		this.PositionY = stream.ReadDouble();
+		this.PositionZ = stream.ReadDouble();
+		this.Value = stream.ReadInt16();
+	}
+
+	public void Write(PacketStream stream)
+	{
+		stream.WriteVarInt(this.Id.Length + this.EntityID.Length + 26);
+		stream.WriteVarInt(this.Id);
+		stream.WriteVarInt(this.EntityID);
+		stream.WriteDouble(this.PositionX);
+		stream.WriteDouble(this.PositionY);
+		stream.WriteDouble(this.PositionZ);
+		stream.WriteInt16(this.Value);
+	}
+}
---
+++ src/Helium.Network.Api.Vanilla/V15/Packets/Clientbound/Play/SpawnWeatherEntityPacket.cs
@@ -0,0 +1,62 @@
+﻿namespace Helium.Network.Api.Vanilla.V15.Packets.Clientbound.Play;
+
+using System;
+
+using Helium.Api.Mojang;
+
+/// <summary>
+/// C0x02 - Clientbound Spawn Weather Entity Packet.
+/// This packet notifies the client of a thunderbolt strike within 512 blocks of the player.
+/// </summary>
+
+// why 512 blocks? there's absolutely no reason to notify the client of a thunderbolt strike in an area that isnt even sent to this client
+// also why does this contain the Y coordinate? lightning bolts always strike the highest block in their xz column
+public struct SpawnWeatherEntityPacket : IPacket
+{
+	public VarInt Id => 0x02;
+
+	/// <summary>
+	/// Serverside thunderbolt entity ID.
+	/// </summary>
+	public VarInt EntityID { get; set; }
+
+	/// <summary>
+	/// Target coordinates of this thunderbolt strike.
+	/// </summary>
+	public Double TargetX { get; set; }
+
+	/// <summary>
+	/// Target coordinates of this thunderbolt strike.
+	/// </summary>
+	public Double TargetY { get; set; }
+
+	/// <summary>
+	/// Target coordinates of this thunderbolt strike.
+	/// </summary>
+	public Double TargetZ { get; set; }
+
+	/// <summary>
+	/// Global weather entity type, always 1
+	/// </summary>
+	public Byte Type { get; set; } // Mojang tried to support having more weather entities here. i suppose it makes sense to keep in for mods
+
+	public void Read(PacketStream stream)
+	{
+		this.EntityID = stream.ReadVarInt();
+		this.Type = stream.ReadUnsignedByte();
+		this.TargetX = stream.ReadDouble();
+		this.TargetY = stream.ReadDouble();
+		this.TargetZ = stream.ReadDouble();
+	}
+
+	public void Write(PacketStream stream)
+	{
+		stream.WriteVarInt(this.Id.Length + this.EntityID.Length + 25);
+		stream.WriteVarInt(this.Id);
+		stream.WriteVarInt(this.EntityID);
+		stream.WriteByte(this.Type);
+		stream.WriteDouble(this.TargetX);
+		stream.WriteDouble(this.TargetY);
+		stream.WriteDouble(this.TargetZ);
+	}
+}
