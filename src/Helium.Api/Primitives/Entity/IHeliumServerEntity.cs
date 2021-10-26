da1cb009166dca9b5101e8c588198e3fde3917e9
ExaInsanity <irisfeanora@outlook.com>
2021-10-26 14:05:49 +0200

    implement C0x00: SpawnEntityPacket

---
+++ src/Helium.Api/Primitives/Entity/IHeliumServerEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Helium.Api.Primitives.Entity;
+
+using System;
+
+internal interface IHeliumServerEntity
+{
+	public Int64 Id { get; }
+	public String Namespace { get; }
+	public String EntityName { get; }
+}
