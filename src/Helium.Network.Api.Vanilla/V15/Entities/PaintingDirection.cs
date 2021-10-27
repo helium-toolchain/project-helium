9b187efcb38781a3de6fa107f604f45d899842b6
ExaInsanity <irisfeanora@outlook.com>
2021-10-27 15:21:12 +0200

    add C0x04

---
+++ src/Helium.Network.Api.Vanilla/V15/Entities/PaintingDirection.cs
@@ -0,0 +1,15 @@
+﻿namespace Helium.Network.Api.Vanilla.V15.Entities;
+
+using Helium.Network.Api.Vanilla.V15.Packets.Clientbound.Play;
+
+/// <summary>
+/// Painting directions as per <see cref="SpawnPaintingPacket.Direction"/>.
+/// </summary>
+public enum PaintingDirection
+{
+	South,
+	West,
+	North,
+	East
+}
+
