199d7f6870ec50e81876c41aac8c97df57d439f1
ExaInsanity <irisfeanora@outlook.com>
2021-11-05 16:03:53 +0100

    add big-endian int32 and int64 types
    this accounts for block reinterpret casting and will be integrated into BinaryNbtReader

---
+++ src/Helium.Nbt/Internal/Int64BigEndian.cs
@@ -0,0 +1,28 @@
+﻿namespace Helium.Nbt.Internal;
+
+using System;
+using System.Buffers.Binary;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Explicit, Size = 64)]
+public struct Int64BigEndian
+{
+	[FieldOffset(0)]
+	public Int64 Value;
+
+	[FieldOffset(0)]
+	public Memory<Byte> AsByte;
+
+	public Int64 AsSystemDefault()
+	{
+		return BinaryPrimitives.ReadInt64BigEndian(AsByte.Span);
+	}
+
+	public void FromSystemDefault(Int64 value)
+	{
+		Span<Byte> m = stackalloc Byte[8];
+		BinaryPrimitives.WriteInt64BigEndian(m, value);
+
+		this.Value = MemoryMarshal.Cast<Byte, Int64>(m)[0];
+	}
+}
