da650b54a6775c901605585050f94aa4ed61e67a
ExaInsanity <irisfeanora@outlook.com>
2022-01-24 19:57:56 +0100

    Add base interfaces

---
+++ src/Helium.Data.Abstraction/IRootToken.cs
@@ -0,0 +1,16 @@
+﻿namespace Helium.Data.Abstraction;
+
+using System;
+using System.Runtime.Versioning;
+
+/// <summary>
+/// Represents the root token of the data structure.
+/// </summary>
+[RequiresPreviewFeatures]
+public interface IRootToken : ICompoundToken
+{
+	/// <summary>
+	/// Notes which data structure this token was created as; to avoid using interfaces for obtaining types.
+	/// </summary>
+	public abstract static String DataFormat { get; }
+}
