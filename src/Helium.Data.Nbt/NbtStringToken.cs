906766c02c57a89ba7ec4ccc2472400ef7d750b6
ExaInsanity <irisfeanora@outlook.com>
2022-01-27 13:05:57 +0100

    implement string token

---
+++ src/Helium.Data.Nbt/NbtStringToken.cs
@@ -0,0 +1,43 @@
+﻿namespace Helium.Data.Nbt;
+
+using System;
+using System.Runtime.Versioning;
+
+using Helium.Data.Abstraction;
+
+/// <summary>
+/// Represents a string token.
+/// </summary>
+[RequiresPreviewFeatures]
+public record struct NbtStringToken : IValueToken<String>
+{
+	/// <summary>
+	/// Stores the binary marker for this token type.
+	/// </summary>
+	public static Byte Declarator => 0x08;
+
+	/// <summary>
+	/// Stores the value of this token.
+	/// </summary>
+	public String Value { get; set; }
+
+	/// <summary>
+	/// Provides an instance access field for this token type.
+	/// </summary>
+	public Byte RefDeclarator => Declarator;
+
+	/// <summary>
+	/// The name of this nbt token.
+	/// </summary>
+	public String Name { get; set; }
+
+	/// <summary>
+	/// The root token of this tree.
+	/// </summary>
+	public IRootToken? RootToken { get; set; }
+
+	/// <summary>
+	/// The immediate parent for this token.
+	/// </summary>
+	public IDataToken? ParentToken { get; set; }
+}
