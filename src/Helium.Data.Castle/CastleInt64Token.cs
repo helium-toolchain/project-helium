5120a56d566031673b67ebca7bd9a72e650cfe60
ExaInsanity <irisfeanora@outlook.com>
2022-02-11 15:54:26 +0100

    implement CastleInt64Token

---
+++ src/Helium.Data.Castle/CastleInt64Token.cs
@@ -0,0 +1,49 @@
+namespace Helium.Data.Castle;
+
+using System;
+using System.Runtime.Versioning;
+
+using Helium.Data.Abstraction;
+using Helium.Data.Nbt;
+
+[RequiresPreviewFeatures]
+public record struct CastleInt64Token : ICastleToken, IValueToken<Int64>
+{
+	public static Byte Declarator => 0x05;
+
+	public UInt16 NameId { get; internal set; }
+
+	public Int64 Value { get; set; }
+
+	public Byte RefDeclarator => Declarator;
+
+	public String Name
+	{
+		get => (this.RootToken as CastleRootToken)?.TokenNames[NameId]!;
+		set
+		{
+			CastleRootToken root = this.RootToken as CastleRootToken ?? throw new ArgumentException(
+				$"Root token of CastleInt64Token {NameId}:{Value} was not of type CastleRootToken");
+
+			if(!root.TokenNames.Contains(value))
+			{
+				root.TokenNames.Add(value);
+			}
+
+			this.NameId = (UInt16)root.TokenNames.IndexOf(value);
+		}
+	}
+
+	public IRootToken? RootToken { get; init; }
+
+	public IDataToken? ParentToken { get; set; }
+
+	public IDataToken ToNbtToken()
+	{
+		return new NbtInt64Token
+		{
+			Name = this.Name,
+			Value = this.Value
+		};
+	}
+}
